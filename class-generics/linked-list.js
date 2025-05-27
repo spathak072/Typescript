3e2e1a89b6c09409fa61beec3d5f3fa74b935a78
spathak072 <saurabhpathak072@gmail.com>
2025-05-27 20:53:28 +0530

    creating list and node classes

---
+++ class-generics/linked-list.js
@@ -0,0 +1,10 @@
+"use strict";
+class ListNode {
+    constructor() {
+    }
+}
+class LinkedList {
+    root; // Optional property
+    length = 0;
+}
+const list = new LinkedList();
---
+++ class-generics/linked-list.ts
@@ -0,0 +1,13 @@
+class ListNode {
+    constructor() {
+        
+    }
+}
+class LinkedList {
+    private root?:ListNode; // Optional property
+    private length = 0;
+
+}
+
+const list = new LinkedList();
+
