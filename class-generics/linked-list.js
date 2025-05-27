5a4bf3a30bfc06195941eb83b6e1348d86c8239d
spathak072 <saurabhpathak072@gmail.com>
2025-05-27 21:28:48 +0530

    Adding a Add method

--- class-generics/linked-list.js
+++ class-generics/linked-list.js
@@ -1,10 +1,28 @@
 "use strict";
 class ListNode {
-    constructor() {
+    value;
+    next; // Optional property
+    constructor(value) {
+        this.value = value;
     }
 }
 class LinkedList {
     root; // Optional property
     length = 0;
+    add(value) {
+        const node = new ListNode(value);
+        if (!this.root) {
+            this.root = node;
+            this.length++;
+            return;
+        }
+        let current = this.root;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = node;
+        this.length++;
+    }
 }
-const list = new LinkedList();
+const numberList = new LinkedList();
+const nameList = new LinkedList();
--- class-generics/linked-list.ts
+++ class-generics/linked-list.ts
@@ -1,14 +1,26 @@
-class ListNode {
-    constructor() {
+class ListNode<T> {
+    next?: ListNode<T>; // Optional property
+    constructor(public value:T) {
         
     }
 }
 class LinkedList<T> {
-    private root?:ListNode; // Optional property
+    private root?:ListNode<T>; // Optional property
     private length = 0;
 
     add(value:T): void {
-       
+       const node = new ListNode(value);
+         if (!this.root) {
+            this.root = node;
+            this.length++;
+            return;     
+         }
+         let current = this.root;
+         while (current.next) {
+            current = current.next;
+         }
+            current.next = node;
+            this.length++;
     }
 
 }
