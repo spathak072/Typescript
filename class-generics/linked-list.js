d7e5554e57235138762b645b150c9e0d9b72ac3d
spathak072 <saurabhpathak072@gmail.com>
2025-05-28 21:32:51 +0530

    Finished Linked List

--- class-generics/linked-list.js
+++ class-generics/linked-list.js
@@ -25,6 +25,77 @@ class LinkedList {
         this.tail = node; // Update tail to the new node
         this.length++;
     }
+
+    insertAt(index, value) {
+        if (index < 0 || index > this.length) {
+            throw new Error("Index out of bounds");
+        }
+        const node = new ListNode(value);
+        if (index === 0) {
+            node.next = this.root;
+            this.root = node;
+            if (!this.tail) {
+                this.tail = node; // If the list was empty, set tail to the new node
+            }
+        } else {
+            let current = this.root;
+            for (let i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            node.next = current.next;
+            current.next = node;
+            if (node.next === null) {
+                this.tail = node; // Update tail if we inserted at the end
+            }
+        }
+        this.length++;
+    }
+    removeAt(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error("Index out of bounds");
+        }
+        if (index === 0) {
+            this.root = this.root.next;
+            if (!this.root) {
+                this.tail = null; // If the list is now empty, set tail to null
+            }
+        } else {
+            let current = this.root;
+            for (let i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next ? current.next.next : null;
+            if (current.next === null) {
+                this.tail = current; // Update tail if we removed the last node
+            }
+        }
+        this.length--;
+    }
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            throw new Error("Index out of bounds");
+        }
+        let current = this.root;
+        for (let i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
+    }
+    set(index, value) {
+        if (index < 0 || index >= this.length) {
+            throw new Error("Index out of bounds");
+        }
+        let current = this.root;
+        for (let i = 0; i < index; i++) {
+            current = current.next;
+        }
+        current.value = value;
+    }
+    clear() {
+        this.root = null;
+        this.tail = null;
+        this.length = 0;
+    }
     print() {
         let current = this.root;
         while (current) {
--- class-generics/linked-list.ts
+++ class-generics/linked-list.ts
@@ -1,48 +1,103 @@
 class ListNode<T> {
-    next?: ListNode<T>; // Optional property
-    constructor(public value:T) {
-        
-    }
-}
-class LinkedList<T> {
-    private root?:ListNode<T>; // Optional property
-    private tail?:ListNode<T>; // Optional property
+    next?: ListNode<T>;
+  
+    constructor(public value: T) {}
+  }
+  
+  class LinkedList<T> {
+    private root?: ListNode<T>;
+    private tail?: ListNode<T>;
     private length = 0;
-
-    getLength(): number {
-        return this.length;
+  
+    add(value: T) {
+      const node = new ListNode(value);
+      if (!this.root || !this.tail) {
+        this.root = node;
+        this.tail = node;
+      } else {
+        this.tail.next = node;
+        this.tail = node;
+      }
+      this.length++;
     }
-
-    
-
-    add(value:T): void {
-       const node = new ListNode(value);
-         if (!this.root) {
-            this.root = node;
-            this.tail = node;
-            this.length++;
-            return;     
-         }
-        this.tail!.next = node; // Use non-null assertion operator
-        this.tail = node; // Update tail to the new node
-            this.length++;
-    }
-
-    print(){
+  
+    insertAt(value: T, pos: number) {
+      if (pos > -1 && pos < this.length && this.root) {
         let current = this.root;
-        while (current) {
-            console.log(current.value);
+        let index = 0;
+        let previous = current;
+        let node = new ListNode(value);
+  
+        if (pos === 0) {
+          node.next = this.root;
+          this.root = node;
+        } else {
+          while (index++ < pos && current.next) {
+            previous = current;
             current = current.next;
+          }
+          node.next = current;
+          previous.next = node;
         }
+        this.length++;
+        return true;
+      } else {
+        return false;
+      }
     }
-
-}
-
-const numberList = new LinkedList<number>();
-numberList.add(1);
-numberList.add(2);
-numberList.add(3);
-numberList.print();
-console.log("Length of numberList:", numberList.getLength());
-const nameList = new LinkedList<string>();
-
+  
+    removeAt(pos: number) {
+      if (pos > -1 && pos < this.length && this.root) {
+        let current = this.root;
+        let previous: ListNode<T> = current;
+        let index = 0;
+  
+        if (pos === 0) {
+          this.root = current.next;
+        } else {
+          while (index++ < pos && current.next) {
+            previous = current;
+            current = current.next;
+          }
+          previous.next = current.next;
+        }
+        this.length--;
+        return current;
+      } else {
+        return null;
+      }
+    }
+  
+    getNumberOfElements() {
+      return this.length;
+    }
+  
+    print() {
+      let current = this.root;
+      while (current) {
+        console.log(current.value);
+        current = current.next;
+      }
+    }
+  }
+  
+  const numberList = new LinkedList<number>();
+  
+  numberList.add(10);
+  numberList.add(5);
+  numberList.add(-3);
+  
+  console.log('Length: ' + numberList.getNumberOfElements());
+  numberList.print();
+  
+  console.log('--- NOW REMOVING INDEX 1 ---');
+  numberList.removeAt(1);
+  console.log('Length: ' + numberList.getNumberOfElements());
+  numberList.print();
+  
+  console.log('--- NOW INSERTING AT INDEX 1 ---');
+  numberList.insertAt(100, 1);
+  console.log('Length: ' + numberList.getNumberOfElements());
+  numberList.print();
+  
+  const nameList = new LinkedList<string>();
