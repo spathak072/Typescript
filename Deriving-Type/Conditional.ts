486ba60a6f13872a15f5dea39e0d1c77a0d97c45
spathak072 <saurabhpathak072@gmail.com>
2025-05-30 01:43:31 +0530

    Conditional Type example

--- Deriving-Type/Conditional.ts
+++ Deriving-Type/Conditional.ts
@@ -10,4 +10,21 @@ let text = 1;
 
 type GetElementType<T> = T extends any[] ? T[number] : never;
 type Element2 = GetElementType<StringArray>; // string
-type Element3 = GetElementType<typeof text>; // string
+type Element3 = GetElementType<typeof text>; // never
+
+type FullnamePerson = {
+    firstName: string;
+    lastName: string;
+}
+type FullnameOrNothingPerson<T> = T extends FullnamePerson ? string : never;
+function getFullName<T extends object>(person:T):FullnameOrNothingPerson<T> {
+    if ("firstName" in person && "lastName" in person && person.firstName && person.lastName) {
+        return `${person.firstName} ${person.lastName}` as FullnameOrNothingPerson<T>;
+    }
+    throw new Error("Person does not have firstName and lastName properties");
+}
+
+const name01 = getFullName({})
+const name02 = getFullName({ firstName: "John", lastName: "Doe" }); // "John Doe"
+const name03 = getFullName({ firstName: "Jane" }); // Error: Person does not have firstName and lastName properties
+const name04 = getFullName({ lastName: "Doe" }); // Error: Person does not have firstName and lastName properties
