cd1d03b9e4c41ae558b406b443f440815de2240d
spathak-rccl-org <spathak@rccl.com>
2025-05-30 14:59:12 +0530

    Building Decorator that Edit class

--- ECMAScript Decorator/decorators.ts
+++ ECMAScript Decorator/decorators.ts
@@ -1,10 +1,13 @@
 // ECMA Script Decorator
 // Decorator are just JS Functions
-function logger(target: any, ctx: ClassDecoratorContext) {
+function logger<T extends new (...args:any[])=>any>(target: T, ctx: ClassDecoratorContext) {
   console.log("Logger Decorator called");
   console.log("Target:", target);
   console.log("Context:", ctx);
     // You can modify the target or add properties
+    return class extends target {
+        age= 30; // Adding a new property to the class
+    }
     
 }
 
@@ -16,3 +19,9 @@ class Person {
         console.log(`Hello, ${this.name}`);
     }
 }
+
+const person = new Person();
+person.greet(); // Hello, Max
+
+console.log("Person instance:", person);        // Person instance: Person { name: 'Max', age: 30 }
+
