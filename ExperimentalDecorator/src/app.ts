05c8d002f1986ec6898ce93b9a2706e9cb7267b1
spathak072 <saurabhpathak072@gmail.com>
2025-05-30 22:36:06 +0530

    property decorator

--- ExperimentalDecorator/src/app.ts
+++ ExperimentalDecorator/src/app.ts
@@ -29,5 +29,34 @@ class Person {
   }
 }
 
+function Log(target:any, propertyName:string | symbol){
+    console.log('Property decorator called');
+    console.log('target:', target);
+    console.log('propertyName:', propertyName);
+}
+
+class Product{
+    @Log
+    title: string;
+    private _price: number;
+    constructor(t: string, p: number) {
+        this._price = p;
+        this.title = t;
+        console.log('Creating Product instance');
+    }
+
+    setPrice(price: number) {
+        if (price < 0) {
+            throw new Error('Invalid price - should be positive');
+        }
+        this._price = price;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax);
+
+    }
+}
+
 const pers = new Person();
 console.log('pers', pers.name); // "John Doe"
